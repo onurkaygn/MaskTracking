82d2f018e3b6a9c205cd46285e816b0fe2f7cdba
Onuralp Kaygın <onuralpkayginedu@gmail.com>
2024-02-15 15:10:22 +0300

    project finished

--- engindemirogcsharp/Program.cs
+++ engindemirogcsharp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace engindemirogcsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "ONURALP";
+            person1.LastName = "KAYGIN";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 34723851250;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+        }
+    }
+}
